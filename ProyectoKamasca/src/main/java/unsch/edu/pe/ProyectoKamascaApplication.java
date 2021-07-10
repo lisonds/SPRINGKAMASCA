af6ab5eef59e5ba511e257d0b2fe399b4e901f6e
Tulio lison <35206029+lisonds@users.noreply.github.com>
2021-07-09 22:36:24 -0500

    creando_Proyecto
    CREANDO PROYECTO sPRING BOOT

---
+++ ProyectoKamasca/src/main/java/unsch/edu/pe/ProyectoKamascaApplication.java
@@ -0,0 +1,13 @@
+package unsch.edu.pe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoKamascaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoKamascaApplication.class, args);
+	}
+
+}
---
+++ ProyectoKamasca/src/test/java/unsch/edu/pe/ProyectoKamascaApplicationTests.java
@@ -0,0 +1,13 @@
+package unsch.edu.pe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoKamascaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
