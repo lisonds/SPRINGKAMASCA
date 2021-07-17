f9e5a5784c209ea0b219f4926111811df2d66c7d
Tulio lison <35206029+lisonds@users.noreply.github.com>
2021-07-17 16:57:59 -0500

    2

--- ProyectoKamasca/src/main/java/unsch/edu/pe/Controller/MaterialesController.java
+++ ProyectoKamasca/src/main/java/unsch/edu/pe/Controller/MaterialesController.java
@@ -1,16 +1,41 @@
 package unsch.edu.pe.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import unsch.edu.pe.model.entity.Materiales;
+import unsch.edu.pe.service.IMaterialesService;
+
+
 @Controller
 @RequestMapping(value = "/panel")
 public class MaterialesController {
-
-	@GetMapping("/materiales")
+	@Autowired
+	private IMaterialesService MateServi;
+	
+	
 	public String Diseniadores() {
 
+		return "views/materiales";
+	}
+	
+	@GetMapping("/materiales")
+	private String Listar() {
+		List<Materiales> materiales=MateServi.buscarTodas();
+		for(Materiales c:materiales) {
+			System.out.println(c.getIdmateriales());
+			System.out.println(c.getNombre());
+			System.out.println(c.getAncho());
+			System.out.println(c.getLargo());
+			System.out.println(c.getImagen());
+		
+		
+		}
+		
 		return "views/materiales";
 	}
 	
