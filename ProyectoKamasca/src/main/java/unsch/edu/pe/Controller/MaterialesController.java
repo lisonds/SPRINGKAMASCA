53a7fa08a21eed66635986ba1181775d567d1f31
Tulio lison <35206029+lisonds@users.noreply.github.com>
2021-07-17 17:37:15 -0500

    3

--- ProyectoKamasca/src/main/java/unsch/edu/pe/Controller/MaterialesController.java
+++ ProyectoKamasca/src/main/java/unsch/edu/pe/Controller/MaterialesController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -24,7 +25,7 @@ public class MaterialesController {
 	}
 	
 	@GetMapping("/materiales")
-	private String Listar() {
+	private String Listar(Model model) {
 		List<Materiales> materiales=MateServi.buscarTodas();
 		for(Materiales c:materiales) {
 			System.out.println(c.getIdmateriales());
@@ -33,9 +34,9 @@ public class MaterialesController {
 			System.out.println(c.getLargo());
 			System.out.println(c.getImagen());
 		
-		
+			
 		}
-		
+		model.addAttribute("Materiales",materiales);
 		return "views/materiales";
 	}
 	
