d88c91d0d5b0c00146b93aad4e144d5688271306
Tulio lison <35206029+lisonds@users.noreply.github.com>
2021-07-18 03:10:00 -0500

    final

--- ProyectoKamasca/src/main/java/unsch/edu/pe/Controller/MaterialesController.java
+++ ProyectoKamasca/src/main/java/unsch/edu/pe/Controller/MaterialesController.java
@@ -1,12 +1,16 @@
 package unsch.edu.pe.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import unsch.edu.pe.model.entity.Materiales;
 import unsch.edu.pe.service.IMaterialesService;
@@ -18,9 +22,16 @@ public class MaterialesController {
 	@Autowired
 	private IMaterialesService MateServi;
 	
-	
-	public String Diseniadores() {
-
+	@GetMapping("materiales/save")
+	public String Guardar(Materiales materiales) {
+		double lar=materiales.getLargo();
+		System.out.println(materiales.getIdmateriales());
+		System.out.println(materiales.getNombre());
+		System.out.println(materiales.getAncho());
+		System.out.println(lar);
+		System.out.println(materiales.getImagen());
+		
+		//MateServi.guardar(materiales);
 		return "views/materiales";
 	}
 	
@@ -28,11 +39,7 @@ public class MaterialesController {
 	private String Listar(Model model) {
 		List<Materiales> materiales=MateServi.buscarTodas();
 		for(Materiales c:materiales) {
-			System.out.println(c.getIdmateriales());
-			System.out.println(c.getNombre());
-			System.out.println(c.getAncho());
-			System.out.println(c.getLargo());
-			System.out.println(c.getImagen());
+			
 		
 			
 		}
@@ -40,5 +47,22 @@ public class MaterialesController {
 		return "views/materiales";
 	}
 	
+	@GetMapping("/materiales/editar")
+	private String Editar(@RequestParam("idmateriales") int idmateriales,Model model) {
+		System.out.println("Este ID llego ="+idmateriales);
+		Materiales mate =MateServi.buscarPorId(idmateriales);
+		
+		System.out.println("Su ID seleccionado es"+mate.getIdmateriales());
+		System.out.println(mate.getNombre());
+		System.out.println("SE BIENE LO QUE VA ENTRAR");
+		model.addAttribute("materiales", mate);
+	return "views/materialesUsa";
+	}
+	
+	@GetMapping("/materialesUsa")
+	public String MaterialesUso() {
+
+		return "views/materialesUsa";
+	}
 	
 }
